c4182405b6c84e08f611b2f13ef7fe8f8e42df6a
Seth M Morton <seth.m.morton@gmail.com>
2012-06-23 13:59:20 -0400

    Added python package ability

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #! /usr/bin/env python
 
+from os.path import join
+from glob import glob
 from distutils.core import setup, Extension
 from numpy import get_include
 
-setup(name='pynpsolve',
+# Create a list of all the source files
+sourcefiles = [join('extensions', 'pyNPSolve.c')]
+for f in glob(join('src', '*')):
+    sourcefiles.append(f)
+
+# Define the build
+setup(name='npsolve',
       version='0.1',
+      author='Seth M. Morton',
+      author_email='drtuba78@gmail.com',
+      description="Easily solve for a nanoparticle's spectral properties analytically",
+      url='https://github.com/SethMMorton/NPSolve',
       ext_modules=[
-        Extension('pynpsolve', ['extensions/pyNPSolve.c'],
-                  include_dirs=['include', get_include()],
-                  library_dirs=['.'],
-                  libraries=['sNPSolve'],)
+        Extension('npsolve', sourcefiles,
+                  include_dirs=['include', get_include()])
         ])
