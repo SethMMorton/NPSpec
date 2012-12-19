6d4c171398055a76b26dead65246bb1c780d3f01
Seth M Morton <seth.m.morton@gmail.com>
2012-12-18 20:23:25 -0800

    Began use of test fixtures in testNPSolve

--- test/test_npsolve.cpp
+++ test/test_npsolve.cpp
@@ -7,7 +7,7 @@ TEST(SanityTest, NLAMBDA_CHECK) {
 }
 
 // Check that the range of the wavelenths array is correct
-TEST(SanityTest, Wavelenths) {
+TEST(SanityTest, Wavelengths) {
     EXPECT_EQ(wavelengths[0], 200.0);
     EXPECT_EQ(wavelengths[1], 201.0);
     EXPECT_EQ(wavelengths[10], 210.0);
@@ -39,22 +39,45 @@ TEST(SanityTest, MatIndx) {
     EXPECT_EQ(material_index((char*) "Kryptonite"), -1);
 };
 
+class TestSolver : public ::testing::Test {
+ protected:
 
-TEST(TestSolver, Mie1Layer) {
+    virtual void SetUp() { 
+        // Initiallize the variables
+        index1[0] = material_index((char*) "Ag");
+        index2[0] = material_index((char*) "Ag");
+        index2[1] = material_index((char*) "SiO2");
+        index3[0] = material_index((char*) "Ag");
+        index3[1] = material_index((char*) "SiO2");
+        index3[2] = material_index((char*) "TiO2");
+        // Relative radii
+        relative_radius_spheroid1[0][0] = 1.0;
+        relative_radius_spheroid1[0][1] = 1.0;
+        relative_radius_spheroid2[0][0] = 0.6;
+        relative_radius_spheroid2[0][1] = 0.6;
+        relative_radius_spheroid2[1][0] = 0.4;
+        relative_radius_spheroid2[1][1] = 0.4;
+        relative_radius_spheroid3[0][0] = 0.2;
+        relative_radius_spheroid3[0][1] = 0.2;
+        relative_radius_spheroid3[1][0] = 0.3;
+        relative_radius_spheroid3[1][1] = 0.3;
+        relative_radius_spheroid3[2][0] = 0.5;
+        relative_radius_spheroid3[2][1] = 0.5;
+    }
+
+    int index1[1], index2[2], index3[3];
+    double relative_radius_spheroid1[1][2];
+    double relative_radius_spheroid2[2][2];
+    double relative_radius_spheroid3[3][2];
+    double qext[NLAMBDA], qabs[NLAMBDA], qscat[NLAMBDA];
+
+};
+
+TEST_F(TestSolver, Mie1Layer) {
     const int nlayers = 1;
-    // Let's look at Ag
-    const int index[nlayers] = { material_index((char*) "Ag") };
-    // The dielectric of the medium is 1.0;
     const double medium_dielectric = 1.0;
-    // Let's say 20 nm radius
     const double radius[2] = { 20.0, -1.0 };
-    // Only one layer, so relative radius is 1.0
-    const double relative_radius_spheroid[nlayers][2] = { { 1.0, 1.0 } };
-    // Now run the calculation
-    double qext[NLAMBDA];
-    double qabs[NLAMBDA];
-    double qscat[NLAMBDA];
-    int result = npsolve(nlayers, radius, relative_radius_spheroid, index,
+    int result = npsolve(nlayers, radius, relative_radius_spheroid1, index1,
                          medium_dielectric, false, false, 1.0, 1.0, Efficiency,
                          qext, qscat, qabs);
     //int i;
@@ -68,17 +91,17 @@ TEST(TestSolver, Mie1Layer) {
     // Checks
     EXPECT_EQ(result, 0);
     // Extinction
-    EXPECT_NEAR(qext[0],   2.2285778886683643, 1e-10);
-    EXPECT_NEAR(qext[250], 0.2386953127709600, 1e-10);
-    EXPECT_NEAR(qext[500], 0.0133494242710007, 1e-10);
+    EXPECT_NEAR(qext[0],   2.2285778886683643, 1e-14);
+    EXPECT_NEAR(qext[250], 0.2386953127709600, 1e-14);
+    EXPECT_NEAR(qext[500], 0.0133494242710007, 1e-14);
     // Scattering
-    EXPECT_NEAR(qscat[0],   0.3062750316703077, 1e-10);
-    EXPECT_NEAR(qscat[250], 0.0572390734717247, 1e-10);
-    EXPECT_NEAR(qscat[500], 0.0039184205430821, 1e-10);
+    EXPECT_NEAR(qscat[0],   0.3062750316703077, 1e-14);
+    EXPECT_NEAR(qscat[250], 0.0572390734717247, 1e-14);
+    EXPECT_NEAR(qscat[500], 0.0039184205430821, 1e-14);
     // Absorption
-    EXPECT_NEAR(qabs[0],   1.9223028569980565, 1e-10);
-    EXPECT_NEAR(qabs[250], 0.1814562392992353, 1e-10);
-    EXPECT_NEAR(qabs[500], 0.0094310037279186, 1e-10);
+    EXPECT_NEAR(qabs[0],   1.9223028569980565, 1e-14);
+    EXPECT_NEAR(qabs[250], 0.1814562392992353, 1e-14);
+    EXPECT_NEAR(qabs[500], 0.0094310037279186, 1e-14);
 };
 
 // Run the tests
