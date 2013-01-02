4216fd19c3616cc08c99ac12e1862e2f0ad39d02
Seth M Morton <seth.m.morton@gmail.com>
2013-01-01 22:57:36 -0500

    Prepped setup.py for distribution and moved distclean here

--- setup.py
+++ setup.py
@@ -3,26 +3,51 @@
 # In case setuptools is not installed
 from distribute_setup import use_setuptools
 use_setuptools()
+from setuptools import setup, Extension
+from setuphelp import current_version, PyTest, Clean
 
 from os.path import join
 from glob import glob
-from setuptools import setup, Extension
 import numpy
 
+DESCRIPTION = "Easily solve for a nanoparticle's spectral properties analytically"
+try:
+    with open('README.rst') as fl:
+        LONG_DESCRIPTION = fl.read()
+except IOError:
+    LONG_DESCRIPTION = DESCRIPTION
+
 # Create a list of all the source files
-sourcefiles = ([join('src', 'python','pyNPSolve.c')] 
+sourcefiles = ([join('src', 'python','pyNPSpec.c')] 
              + glob(join('src', '*.cpp')))
 
 # Define the build
-setup(name='npsolve',
-      version='0.1',
+setup(name='npspec',
+      version=current_version(),
       author='Seth M. Morton',
       author_email='drtuba78@gmail.com',
-      description="Easily solve for a nanoparticle's spectral properties analytically",
-      url='https://github.com/SethMMorton/NPSolve',
+      url='https://github.com/SethMMorton/NPSpec',
+      license='MIT',
       ext_modules=[
-        Extension('npsolve', sourcefiles,
+        Extension('npspec', sourcefiles,
                   include_dirs=['include', numpy.get_include()],
                  )
         ],
-      )
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      use_2to3=True,
+      tests_require=['pytest', 'sphinx'],
+      cmdclass={'test':PyTest, 'distclean':Clean},
+      classifiers=(
+                  'Development Status :: 4 - Beta',
+                  #'Development Status :: 5 - Production/Stable',
+                  'Intended Audience :: Science/Research',
+                  'Operating System :: OS Independent',
+                  'License :: OSI Approved :: MIT License',
+                  'Natural Language :: English',
+                  #'Programming Language :: Python :: 2.6',
+                  #'Programming Language :: Python :: 2.7',
+                  #'Programming Language :: Python :: 3',
+                  'Topic :: Scientific/Engineering',
+     )
+    )
