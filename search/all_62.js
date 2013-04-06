3603978ac49e65440ca4f480ee19c4cdb0dd14af
Seth M Morton <seth.m.morton@gmail.com>
2013-04-05 20:27:00 -0700

    Added build instructions

--- search/all_62.js
+++ search/all_62.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['building_2c_20installing_2c_20and_20developing_20with_20npspec',['Building, Installing, and Developing with NPSpec',['../buildpage.html',1,'']]],
   ['bibliographic_20references',['Bibliographic References',['../citelist.html',1,'']]]
 ];
